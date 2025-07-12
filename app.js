44dc98ceea37703e62fea01346afe418576ffc06
SANNITH <sannithnalluri@gmail.com>
2025-07-12 22:06:43 +0530

    app.js update

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT | 8000;
+const port = process.env.PORT || 8000;
 
 const authRoute = require('./routes/authRoute');
 const adminRoute = require('./AdminFunction/AdminRoutes');
-const PassengerRoute = require('./routes/PassengerRoute'); // <- Import the passenger route
-
-// <- Import the admin route
-const db = require('./models'); // <- Import this here
+const PassengerRoute = require('./routes/PassengerRoute');
+const db = require('./models');
 const cors = require('cors');
 
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.use("/api", authRoute);
 app.use("/admin", adminRoute);
-app.use('/passenger',PassengerRoute); // <- Use the admin route
-
+app.use("/passenger", PassengerRoute);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// Start Server After DB Sync
 db.sequelize.sync({})
   .then(() => {
     console.log('Database synced');
-    app.listen(port, () => {
-      console.log(`server started Successfully:${port}`);
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Server started successfully on port: ${port}`);
     });
   })
   .catch((err) => {
