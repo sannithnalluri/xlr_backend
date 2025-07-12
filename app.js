bf06f1f4a0f481656c2892db8ef7a7f460305f9d
SANNITH <sannithnalluri@gmail.com>
2025-07-12 22:05:06 +0530

    pm2 update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.eventNames.PORT | 8000;
+const port = process.env.PORT | 8000;
 
 const authRoute = require('./routes/authRoute');
 const adminRoute = require('./AdminFunction/AdminRoutes');
